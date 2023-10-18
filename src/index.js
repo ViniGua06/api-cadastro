9f5669e69b9e436a047061ba5aafc61446a33f71
Vinicius Guarnieri <viniguarnierisouza@gmail.com>
2023-10-18 00:28:27 -0300

    agora vai confia

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const db = require("./db");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-app.use(express.json());
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
